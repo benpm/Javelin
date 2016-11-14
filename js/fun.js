7df670064a0962555f8f7a6d7befcfc2bf4ad19e
Benjamin M <schwalegos@gmail.com>
2016-11-14 14:38:25 -0700

    next

--- js/fun.js
+++ js/fun.js
@@ -67,47 +67,61 @@ var renderer = new THREE.WebGLRenderer({ antialias:true });
 
 //Set Renderer Things
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0xffffff, 1);
 document.body.appendChild(renderer.domElement)
 
 //Definitions
 var debugText = document.getElementById("debug");
 
 //Add Cube
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({wireframe: true, opacity: 0.5, transparent: true, color: 0x00aaff});
+var geometry = new THREE.BoxGeometry(3, 3, 3);
+var material = new THREE.MeshBasicMaterial({color: 0x333333});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 //Add Arrow
 var arrow = new THREE.ArrowHelper(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), 1, 0xffffff, 0.5, 0.25);
+arrow.line.material.color.setHex(0x000000);
+arrow.cone.material.color.setHex(0x000000);
 scene.add(arrow);
+
+//Add Circle
+var circle = new THREE.Shape();
+circle.moveTo(0, 1);
+circle.quadraticCurveTo(1, 1, 1, 0);
+circle.quadraticCurveTo(1, -1, 0, -1);
+circle.quadraticCurveTo(-1, -1, -1, 0);
+circle.quadraticCurveTo(-1, 1, 0, 1);
+//scene.add(new THREE.Line(circle, new THREE.LineBasicMaterial({color: 0x000000, linewidth: 1})));
+
 
 //Render
 render();
-	function render() {
-		//Pre-Frame
-		requestAnimationFrame(render);
+function render() {
+	//Pre-Frame
+	requestAnimationFrame(render);
 
-		//Text
-		debugText.innerHTML = "Mouse Position = " + mouse.x + "," + mouse.y
-			+ "<br>Camera Position = " + camera.position.x + "," + camera.position.y
-			+ "<br>Player Position = " + player.pos.x + "," + player.pos.y;
+	//Text
+	debugText.innerHTML = "Mouse Position = " + mouse.x + "," + mouse.y
+		+ "<br>Camera Position = " + camera.position.x + "," + camera.position.y
+		+ "<br>Player Position = " + player.pos.x + "," + player.pos.y;
 
-		//Move
-		//cube.position.x = lerp(cube.position.x, mouse.x, 0.1);
-		//cube.position.y = lerp(cube.position.y, mouse.y, 0.1);
-		cube.rotation.y = (cube.position.x + cube.position.y) / 4;
-		player.pos.x += (mouse.x - mouse.lastx) * player.speed;
-		player.pos.y += (mouse.y - mouse.lasty) * player.speed;
-		arrow.rotation.z = angle(0, 0, mouse.x, mouse.y);
-		arrow.position = player.pos;
-		arrow.setLength(dist(mouse.lastx, mouse.lasty, mouse.x, mouse.y), 0.5, 0.25);
+	//Move
+	cube.position = camera.position;
+	cube.rotation.y += 0.05;
+	cube.rotation.z += 0.05;
+	player.pos.x += (mouse.x - mouse.lastx) * player.speed;
+	player.pos.y += (mouse.y - mouse.lasty) * player.speed;
+	arrow.rotation.z = angle(mouse.lastx, mouse.lasty, mouse.x, mouse.y);
+	arrow.position.x = mouse.lastx;
+	arrow.position.y = mouse.lasty;
+	arrow.setLength(dist(mouse.lastx, mouse.lasty, mouse.x, mouse.y) * 1.5, 0.3, 0.1);
 
-		//Translate Camera
-		camera.position.lerp(player.pos, 0.05);
+	//Translate Camera
+	//camera.position.lerp(player.pos, 0.05);
 
-		//Post-Frame
-		renderer.render(scene, camera);
-		mouse.lastx = mouse.x;
-		mouse.lasty = mouse.y;
-	}
+	//Post-Frame
+	renderer.render(scene, camera);
+	mouse.lastx = mouse.x;
+	mouse.lasty = mouse.y;
+}
